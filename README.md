8eaeca605d855d5f91986355e27a720c65623078
Jason Owen <jason@jasonaowen.net>
2022-03-05 20:22:03 -0800

    Implement first draft of wordle solver
    Describe the algorithm for evaluating a single board, and implement that
    algorithm on a hard-coded grid. Running the script produces a JSON list
    of potential answers for that board.
    
    There is a ton of optimization opportunity here, but without having
    carefully checked, I don't think the output is incorrect!

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+# Wordle solver
+
+This program tries to figure out what the word of the day is based on completed
+Wordle grids!
+
+## Algorithm
+
+The program evaluates each grid to determine eligible words.
+A word is a potential answer for a given grid if, for each line, there is at
+least one valid word that would produce the given pattern.
+
+For example, this grid would rule out the word `QUEST`:
+🟨🟨🟨🟨🟨
+🟩🟩🟩🟩🟩
+
+because there aren't any anagrams of `QUEST`.
+
 ![Licensed under the AGPL, version 3](https://img.shields.io/badge/license-AGPL3-blue.svg)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,70 @@
-console.log('Hello, world');
+import answers from './answers.json';
+import allowed from './allowed.json';
+
+const words = [...answers, ...allowed].sort();
+
+// type character = '🟩' | '🟨' | '⬛' | '⬜';
+
+interface Grid {
+  lines: string[];
+//  day?: number;
+//  hard: boolean;
+//  mode: 'light' | 'dark';
+}
+
+const generateRegex = (answer: string, line: string): RegExp => {
+  const answerCharacters = new Set([...answer]);
+  for (let i = 0; i < 5; i++) {
+    if (line.charAt(i) === '🟩') {
+      answerCharacters.delete(answer.charAt(i));
+    }
+  }
+  const answerChars = [...answerCharacters.values()];
+
+  const characterRegexes = [...line].map((character: string, index: number) => {
+    switch (character) {
+      case '🟩':
+        return answer[index];
+      case '🟨':
+        return `[${answerChars.filter((c) => c !== answer.charAt(index)).join('')}]`;
+      case '⬛':
+      case '⬜':
+        return '[^answer]';
+      default:
+        throw new Error('invalid grid character');
+    }
+  });
+  return new RegExp(['^', ...characterRegexes, '$'].join(''));
+};
+
+const matches = (line: string, answer: string, word: string): boolean => (
+  (generateRegex(answer, line)).test(word)
+);
+
+const wordExists = (line: string, answer: string): boolean => (
+  words.some((word) => matches(line, answer, word))
+);
+
+const evaluate = (grid: Grid, answer: string): boolean => (
+  grid.lines.every((line) => wordExists(line, answer))
+);
+
+const possibleAnswers = (grid: Grid): string[] => (
+  answers.filter((answer) => evaluate(grid, answer))
+);
+
+/*
+    '🟩🟩🟩🟩⬜',
+    '🟩🟩🟩🟩⬜',
+    '🟩🟩🟩🟩🟩',
+*/
+// type character = '🟩' | '🟨' | '⬛' | '⬜';
+const grid: Grid = {
+  lines: [
+    '🟩🟩⬜🟨⬜',
+    '🟩🟩🟩⬜⬜',
+    '🟩🟩🟩🟩⬜',
+    '🟩🟩🟩🟩🟩',
+  ],
+};
+console.log(JSON.stringify(possibleAnswers(grid)));
